189e28464e293660d9056aa38784dc103ace3b56
Aasthabarot <barotaastha38@gmail.com>
2024-08-23 19:18:45 +0530

    sitemap and robot.txt

--- src/components/blogs/BlogWidget.jsx
+++ src/components/blogs/BlogWidget.jsx
@@ -1,18 +1,7 @@
-import Image from 'next/image';
 import React from 'react';
-import DOMPurify from 'dompurify';
+import Image from 'next/image';
+import truncate from 'truncate-html'; // Import truncate function from truncate-html
 
-// Function to truncate the HTML content to a certain word limit
-const truncateContent = (htmlContent, wordLimit) => {
-  if (typeof window !== 'undefined') {
-    const div = document.createElement('div');
-    div.innerHTML = DOMPurify.sanitize(htmlContent);
-    const text = div.textContent || div.innerText || '';
-    const words = text.split(' ').slice(0, wordLimit).join(' ');
-    return words + '...';
-  }
-  return htmlContent;
-};
 // Function to format the date to only show the date part
 const formatDate = (dateString) => {
   const date = new Date(dateString);
@@ -22,13 +11,15 @@ const formatDate = (dateString) => {
   return `${month}-${day}-${year}`;
 };
 
-const BlogWidget = ({ featureImage, updatedAt, category, title, content,slug, wordLimit = 30 }) => {
-  const truncatedContent = truncateContent(content, wordLimit);
+const BlogWidget = ({ featureImage, updatedAt, category, title, content, slug, wordLimit = 30 }) => {
+  // Apply the truncation function
+  const truncatedContent = truncate(content, { length: wordLimit * 5 }); // Adjust length as needed
   const formattedDate = formatDate(updatedAt);
   const formattedCategory = category.toUpperCase(); 
+
   return (
     <div className="bg-gray-800 text-white rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 flex flex-col">
-      <div className="relative h-60 w-full ">
+      <div className="relative h-60 w-full">
         <Image width={600} height={600} src={featureImage} alt={title} className="rounded-lg p-1 md:p-2 lg:p-3" />
       </div>
 
@@ -38,7 +29,7 @@ const BlogWidget = ({ featureImage, updatedAt, category, title, content,slug, wo
           <p className="bg-blue-500 text-white py-1 px-2 rounded-xl">{formattedCategory}</p>
         </div>
 
-        <h3 className=" bg-gradient-to-r from-blue-600 to-green-500 text-transparent bg-clip-text font-bold mt-2 mb-2 md:mt-2 md:mb-2 text-xl md:text-2xl lg:text-xl">
+        <h3 className="bg-gradient-to-r from-blue-600 to-green-500 text-transparent bg-clip-text font-bold mt-2 mb-2 md:mt-2 md:mb-2 text-xl md:text-2xl lg:text-xl">
           {title}
         </h3>
         <div className="mb-2 md:text-xl lg:text-[18px]" dangerouslySetInnerHTML={{ __html: truncatedContent }}></div>

1795de1bdd3d9b2be3319cd29f1d06615413163b
Aasthabarot <barotaastha38@gmail.com>
2024-09-12 18:01:05 +0530

    internal page

--- src/app/internship/page.jsx
+++ src/app/internship/page.jsx
@@ -150,7 +150,7 @@ const InternshipPage = () => {
         <span
           className={`bg-gradient-to-r from-blue-600 to-green-500 inline-block text-transparent bg-clip-text`}
         >
-          {"Your growth takes a halt;"}
+           &quot;Your growth takes a halt&quot;
         </span>
         <br />
         <span>You&apos;re not evolving
